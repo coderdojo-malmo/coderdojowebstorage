63184d34b96d3c3262683d31dbf91076812280fe
Joel Hansson <joel.hansson@gmail.com>
2013-02-08 10:35:25 +0100

    adds txt as valid viewable/editable file type

--- lib/filetype_helpers.rb
+++ lib/filetype_helpers.rb
@@ -6,14 +6,15 @@ module Sinatra
       when '.css' then :css
       when '.js' then :js
       when '.png', '.gif', '.jpg', '.jpeg' then :image
+      when '.txt' then :txt
       else :unknown
       end
     end
     def is_editable?(file_type)
-      [:html, :css, :js].include? file_type
+      [:html, :css, :js, :txt].include? file_type
     end
     def is_viewable?(file_type)
-      [:html, :image].include? file_type
+      [:html, :image, :txt].include? file_type
     end
   end
 
